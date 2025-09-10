284948b67ea5630d33b04ecaed7d580f51a56beb
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2025-09-10 13:12:57 -0400

    Add project files.

---
+++ F25W2IntroToMethods/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace F25W2IntroToMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        static void ChangeValue(int num)
+        {
+            num = 100;
+        }
+    }
+}
