5cc1439483044d94740217e98eb08c2ab3f6351b
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2025-09-10 14:00:53 -0400

    output param

--- F25W2IntroToMethods/Program.cs
+++ F25W2IntroToMethods/Program.cs
@@ -2,6 +2,8 @@
 {
     internal class Program
     {
+        //int x;
+
         static void Main(string[] args)
         {
             int n = 5;
@@ -9,19 +11,39 @@
             int[] a = { 1, 2, 3 };
 
             Console.WriteLine("Before changes:");
-            Console.WriteLine("n = " + n);
+            //Console.WriteLine("n = " + n);
             Console.WriteLine("s = " + s);
             Console.WriteLine("a[0] = " + a[0]);
 
-            ChangeValue(n, s, a);
+            ChangeValue(ref n, ref s, a);
 
             Console.WriteLine("\n\nAfter changes:");
             Console.WriteLine("n = " + n);
             Console.WriteLine("s = " + s);
             Console.WriteLine("a[0] = " + a[0]);
+
+            Console.WriteLine("\n\n\n");
+
+
+
+            int rad = 5;
+            //double circum, area;
+
+            Calculate(rad, out double circum, out double area);
+
+            Console.WriteLine("Circumference = " + circum.ToString("F"));
+            Console.WriteLine("Area = " + area.ToString("F"));
         }
 
-        static void ChangeValue(int num, string str, int[] arr)
+        static void Calculate(int r, out double cir, out double ar)
+        {
+            cir = 2 * Math.PI * r;
+            ar = Math.PI * r * r;
+        }
+
+
+
+        static void ChangeValue(ref int num, ref string str, int[] arr)
         {
             num = 100;
             str = "bye";
